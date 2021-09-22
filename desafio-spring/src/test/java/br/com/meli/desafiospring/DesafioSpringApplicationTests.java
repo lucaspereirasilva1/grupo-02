ec22b61852f13dde4fecb9a8f4102db13922e91e
lucaspereirasilva1 <lucas.lpereira@mercadolivre.com>
2021-09-22 10:13:29 -0300

    commit inicial do projeto

---
+++ desafio-spring/src/test/java/br/com/meli/desafiospring/DesafioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.desafiospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
