d3434646464afc6c17ff88c5c2773d8b73435459
lucaspereirasilva1 <lucas.lpereira@mercadolivre.com>
2021-09-24 14:23:25 -0300

    refactor: implementado regras de validacao para entrada de parametros no cadastro do paciente

--- desafio-spring/src/main/java/br/com/meli/desafiospring/model/entity/Paciente.java
+++ desafio-spring/src/main/java/br/com/meli/desafiospring/model/entity/Paciente.java
@@ -3,15 +3,19 @@ package br.com.meli.desafiospring.model.entity;
 import lombok.Data;
 import lombok.ToString;
 
+import java.time.LocalDate;
+
 @Data
 @ToString
 
 public class Paciente {
+
     private Integer id;
     private String especie;
     private String raca;
     private String cor;
-    private String dataDeNascimento;
+    private LocalDate dataDeNascimento;
     private String nome;
+    private Proprietario proprietario;
 
 }
