e696c16872326df1bf39e0670d288b12f6e7b462
lucaspereirasilva1 <lucas.lpereira@mercadolivre.com>
2021-09-24 20:15:12 -0300

    refactor: refatorado os conversores de DTOs de todos os modulos para somente um no pacote Util

--- desafio-spring/src/main/java/br/com/meli/desafiospring/util/ArquivoUtil.java
+++ desafio-spring/src/main/java/br/com/meli/desafiospring/util/ArquivoUtil.java
@@ -1,9 +1,5 @@
 package br.com.meli.desafiospring.util;
 
-import br.com.meli.desafiospring.model.entity.Consulta;
-import br.com.meli.desafiospring.model.entity.Medico;
-import br.com.meli.desafiospring.model.entity.Paciente;
-import br.com.meli.desafiospring.model.entity.Proprietario;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -12,7 +8,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class ArquivoUtil <T>{
+public class ArquivoUtil<T>{
 
     public  void collectionToJson(File file, List<T> users) throws IOException {
         ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
